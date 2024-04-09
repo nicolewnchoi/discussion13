0f477f5b2387d255038a3ec9edf2e5ebe67d27b5
nicolewnchoi <94572667+nicolewnchoi@users.noreply.github.com>
2024-04-09 17:46:03 -0400

    Add files via upload

---
+++ js/challenge3.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function () {
+  //   alert("loaded");
+  let figcaptions = document.querySelector("figcaption");
+  let figcaption2 = figcaptions[1];
+  this.alert(figcaption2);
+  alert(document.querySelector("figcaption")[1].innerHTML);
+});
